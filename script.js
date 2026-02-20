3ee3aff20244e683339a93df8433fe46f55b0ef8
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-02-20 12:03:23 -0500

    feat: Implement asynchronous weather data fetching and display based on user input, including error handling and dynamic icon updates.

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const searchButton = document.getElementById('searchButton');
-const searchInput = document.getElementById('searchInput');
-const weatherImage = document.getElementById('weatherImage');
 const weatherTemp = document.getElementById('weatherTemp');
 const city = document.getElementById('city');
 
-
+/*
 fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`)
     .then(response => console.log(response.json()))
     .then(data => console.log(data))
     .catch(error => console.log(error));
+*/
+
+fetchData()
+
+searchButton.addEventListener("click", fetchData);
+
+async function fetchData() {
+   
+
+    try{
+        const searchInput = document.getElementById('searchInput').value.toLowerCase();
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchInput}&aqi=no`);
+        if(!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json();
+        console.log(data);
+        const weatherIcon = data.current.condition.icon;
+        const weatherImage = document.getElementById('weatherImage');
+
+        weatherImage.src = weatherIcon;
+    }
+    catch(error){
+        console.error(error);
+    }
+    
+}

caa0bba1eef64b05fe885f1081d40feef91d00eb
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-02-19 23:11:48 -0500

    feat: Add IDs to search input and button in HTML and create `script.js` to select DOM elements.

---
+++ script.js
@@ -0,0 +1,5 @@
+const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+const weatherImage = document.getElementById('weatherImage');
+const weatherTemp = document.getElementById('weatherTemp');
+const city = document.getElementById('city');

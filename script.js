9baf540f24e26b4f83525e9f66c04b7ed6db9134
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-02-19 23:36:31 -0500

    feat: Add initial weather API fetch for London.

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const weatherTemp = document.getElementById('weatherTemp');
 const city = document.getElementById('city');
 
 
+fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`)
+    .then(response => console.log(response.json()))
+    .then(data => console.log(data))
+    .catch(error => console.log(error));

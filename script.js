9f469e5640aa79cb296e320dea69416c3fe81184
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-02-19 23:19:21 -0500

    feat: Add env.js script to index.html and include it in .gitignore.

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const searchInput = document.getElementById('searchInput');
 const weatherImage = document.getElementById('weatherImage');
 const weatherTemp = document.getElementById('weatherTemp');
 const city = document.getElementById('city');
+
+
